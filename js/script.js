0e936e255df8a6c8ece4f3fcc47514dc11ef0d7b
Giulia Cardieri <giulia.cardieri@gmail.com>
2018-04-14 17:16:48 -0300

    Create functionality to save a new event and save local using a fakeDB with json on localhost. Add image for social category.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* user preferences functions */
 var getUser = function getUser() {
 	if (localStorage.getItem('user'))
 		return JSON.parse(localStorage.getItem('user'));
@@ -17,8 +18,8 @@ var setUser = function setUser(user_new) {
       { 'id': 5, 'name': 'events_style', 'value': 1 },
       { 'id': 6, 'name': 'images_card', 'value': 1 },
     ]));
-    userPreferencesDesign();
   }
+  userPreferencesDesign();
 };
 
 var setNewItem = function setNewItem(element, value) {
@@ -29,38 +30,66 @@ var setNewItem = function setNewItem(element, value) {
 }
 
 var userPreferencesDesign = function userPreferencesDesign() {
-	var user = getUser();
+  var user = getUser();
 
 	if (user[0].value == 2) {
 		$('.checkbox-elem--type-material').addClass('hidden');
     $('.checkbox-elem--type-browser-default').removeClass('hidden');
+  } else if (user[0].value == 1){
+    $('.checkbox-elem--type-material').removeClass('hidden');
+    $('.checkbox-elem--type-browser-default').addClass('hidden');
   }
-  if (user[1].value == 2)
-    $('.datepicker-elem').removeClass('datepicker').attr('type', 'date');
+
+  if (user[1].value == 2) {
+    $('.datepicker-elem-group--type-browser-default').removeClass('hidden');
+    $('.datepicker-elem-group--type-material').addClass('hidden');
+  } else if (user[1].value == 1) {
+    $('.datepicker-elem-group--type-browser-default').addClass('hidden');
+    $('.datepicker-elem-group--type-material').removeClass('hidden');
+  }
+
   if (user[2].value == 2) {
     $('.input-elem-group__input').addClass('browser-default');
     $('.input-elem-group__label').addClass('browser-default');
+  } else if (user[2].value == 1) {
+    $('.input-elem-group__input:not(.input-test)').removeClass('browser-default');
+    $('.input-elem-group__label:not(.input-test)').removeClass('browser-default');
   }
+
   if (user[3].value == 2) {
     $('.select-elem').addClass('browser-default');
     $('.select-elem__label').addClass('browser-default');
+  } else if (user[3].value == 1) {
+    $('.select-elem').removeClass('browser-default');
+    $('.select-elem__label').removeClass('browser-default');
   }
+
   if (user[4].value == 2) {
     $('.timepicker-elem-group--type-browser-default').removeClass('hidden');
     $('.timepicker-elem-group--type-material').addClass('hidden');
+  } else if (user[4].value == 1) {
+    $('.timepicker-elem-group--type-browser-default').addClass('hidden');
+    $('.timepicker-elem-group--type-material').removeClass('hidden');
   }
-
-  initializeMaterializeComponents();
 }
 
+var updateChosenElements = function updateChosenElements() {  
+  var user = getUser();
+  for (var i = 0; i<Object.keys(user).length; i++)
+    $('main .btn-choose.' + user[i].name + '-' + user[i].value).trigger('click');
+}
+
+/* handlebars templates functions */
 var loadDBTemplate = function loadDBTemplate(source) {
-  var template, html;
+  var template, html, filtered_db;
   
+  filtered_db = filterJSONByDate(getDB());
   template = Handlebars.compile(source);
-  html = template(db);
+  html = template(sortDB(filtered_db, 'time_start'));
 
   $('.header__tabs').addClass('header__tabs--state-hidden');
   $('main').html(html);
+  initializeMaterializeComponents();
 }
 
 var loadUserPrefsTemplate = function loadUserPrefsTemplate(source) {
@@ -74,21 +103,74 @@ var loadUserPrefsTemplate = function loadUserPrefsTemplate(source) {
 
   rememberTabsContent();
   initializeMaterializeComponents();
+  updateChosenElements();
 }
 
+/* form functions */
+var addFormData = function addFormData(data) {
+  console.log(data);
+  var final_data = {};
+  var user = getUser();
+  var old_db = getDB();
+
+  final_data.title = data[0].value;
+  final_data.location = data[7].value;
+  final_data.category = data[8].value;
+
+  if (user[1].value == 1) {
+    final_data.date = (data[1].value);
+  } else {
+    final_data.date = (data[2].value);
+  }
+
+  if (user[4].value == 1) {
+    final_data.time_start = data[3].value;
+    final_data.time_end = data[4].value;
+  } else {
+    final_data.time_start = data[5].value;
+    final_data.time_end = data[6].value;
+  }
+
+  switch(final_data.category) {
+    case 'Diabetes': final_data.icon = 'local_pharmacy'; break;
+    case 'Exercise': final_data.icon = 'fitness_center'; break;
+    case 'Family': final_data.icon = 'child_care'; break;
+    case 'Food': final_data.icon = 'restaurant'; break;
+    case 'Health': final_data.icon = 'local_hospital'; break;
+    case 'Home': final_data.icon = 'home'; break;
+    case 'Personal': final_data.icon = 'face'; break;
+    case 'Pets': final_data.icon = 'pets'; break;
+    case 'School': final_data.icon = 'school'; break;
+    case 'Shopping': final_data.icon = 'local_mall'; break;
+    case 'Social': final_data.icon = 'people'; break;
+    case 'Other': final_data.icon = 'info'; break;
+    case 'Travel': final_data.icon = 'flight_land'; break;
+    case 'Work': final_data.icon = 'work'; break;
+  }
+  final_data.completed = false;
+  old_db.push(final_data);
+  setDB(old_db);
+}
+
+/* my design page tab menu functions */
 var rememberTabsContent = function rememberTabsContent() {
   var id = $('.header__tabs a.active').attr('href');
   $(id).removeClass('hidden');
 }
 
-var initializeMaterializeComponents = function initializeMaterializeComponents() {
-  $('.datepicker').datepicker();
+/* materialize function */
+var initializeMaterializeComponents = function initializeMaterializeComponents() { 
+  $('.datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    showClearBtn: true,
+  });
   $('.collapsible').collapsible();
   $('select').formSelect();
   $('.timepicker').timepicker();
   $('.tabs').tabs();
 }
 
+/* nav functions */
 var findCurrentNav = function findCurrentNav(id) {
   switch (id) {
     case 'home': loadDBTemplate($('#home-template').html()); break;
@@ -97,13 +179,52 @@ var findCurrentNav = function findCurrentNav(id) {
   }
 }
 
+/* fake db functions*/
+/* a fake db was created in order to save all user data on the device, allowing more privacy */
+var filterJSONByDate = function filterJSONByDate(array) {
+  var today = (new Date()).toISOString().substring(0, 10);
+  var new_db = [];
+
+  for (var i = 0; i<Object.keys(array).length; i++) {
+    if (array[i].date == today)
+      new_db.push(array[i])
+  }
+
+  return new_db;
+}
+
+var sortDB = function sortDB(array, key) {
+  return array.sort(function(a, b) {
+    var x = a[key]; 
+    var y = b[key];
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+
+var setDB = function setUser(new_db) {
+  if (new_db)
+   localStorage.setItem('agenda_db', JSON.stringify(new_db));
+  else {
+    localStorage.setItem('agenda_db', JSON.stringify(db));
+  }
+};
+
+var getDB = function getDB() {
+  if (localStorage.getItem('agenda_db'))
+    return JSON.parse(localStorage.getItem('agenda_db'));
+  return null;
+}
+
 $(function(){
+
+  /* initializations */
 	if (!getUser())
 		setUser();
 
-  userPreferencesDesign();
+  if (!getDB())
+    setDB();
 
-  initializeMaterializeComponents();
+  userPreferencesDesign();
 
   Handlebars.registerHelper('toLowerCase', function(str) {
     return str.toLowerCase();
@@ -111,7 +232,7 @@ $(function(){
 
   loadDBTemplate($('#home-template').html());
 
-
+  /* cards actions */
   $('.card-action__btn--completed').on('click', function() {
     // TODO make this better
     $(this).parent().parent().parent().find('.card.swipe-elem').trigger('swiperight');
@@ -142,6 +263,7 @@ $(function(){
     });
   });
 
+  /* form page actions */
   $('.nav-wrapper__a--type-back').on('click', function() {
     $('.body__content-wrapper').removeClass('hidden');    
     $('.body__add-form-section').animate({
@@ -156,21 +278,28 @@ $(function(){
     $('.footer__nav .li__a#home').trigger('click');
   });
 
+  $('.add-form__form').on('submit', function(e) {
+    e.preventDefault();
+    console.log('submitted!');
+    addFormData($(this).serializeArray());
+    return false;
+  });
 
+  /* footer nav clicks */
   $('.footer__nav .li__a').on('click', function() {
     $('.li__a').removeClass('li__a--state-active');
     $(this).addClass('li__a--state-active');
     findCurrentNav($(this).attr('id'));
   });
 
+  /* tab menu clicks*/
   $('.header__tabs ul li a').on('click', function() {
     $('.tabs__div').addClass('hidden');
     var id = $(this).attr('id');
     $('#' + id).removeClass('hidden');
   });
 
-  /* btns */
-
+  /* btns clicks */
   $('main').on('click', '.main__btn--add', function() {  
     $(this).addClass('hidden'); 
     $('.body__add-form-section').removeClass('hidden');
@@ -183,10 +312,12 @@ $(function(){
 
   $('main').on('click', '.btn-choose', function() {
     setNewItem($(this).attr('data-element'), $(this).attr('data-value'));
+
     $('.btn-choose').removeClass('btn-choose--state-chosen').html('Choose');
     $(this).addClass('btn-choose--state-chosen').html('Chosen');
+
     var icon_elem = $(this).attr('data-icon');
-    $('.collapsible-header__span').addClass('hidden');
+    $('.collapsible-header__span.elem-' + $(this).attr('data-element')).addClass('hidden');
     $('.collapsible-header__span-' + icon_elem).removeClass('hidden');
   });
 });
